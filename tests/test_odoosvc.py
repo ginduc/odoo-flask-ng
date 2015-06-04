829f7804cd2da452f7bb5d5a61643d2915755aab
Gian Inductivo <gian@dynamicobjx.com>
2015-06-04 17:14:51 +0800

    cleanup

--- tests/test_odoosvc.py
+++ tests/test_odoosvc.py
@@ -5,8 +5,6 @@ import pytest
 from sd5 import create_app
 from odoorpc import Connection
 from odoorpc.partner_svc import list_partners
-import logging
-log = logging.getLogger('werkzeug')
 
 class TestOdooSvc:
     def test_sender_lookup(self):
@@ -15,8 +13,6 @@ class TestOdooSvc:
         app = create_app('sd5.settings.TestConfig', env='dev')
         conn = Connection(app.config)
         partners = list_partners(conn)
-
-        app.logger.error("=-=-=-=-=-=-=-=-=-=-> " + str(partners))
 
         assert partners is not None
         assert len(partners) > 0
